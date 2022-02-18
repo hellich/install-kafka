1dc618f8c677d325a749ef3a96fba0df84f3179e
helmi <hichrihelmi@gmail.com>
2022-02-18 14:29:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
+if [ $# -eq 0 ]
+then
+echo "No arguments supplied"
+else
 hostname=$1
-cd /SERVICE/kafka/kafka_2.12-2.4.1
-sed -i 's/xxx/xxx/g' config/server.properties
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
+
+sudo cd /SERVICE/kafka/kafka_2.12-2.4.1
+sudo sed -i 's/xxx/xxx/g' config/server.properties
+sudo nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
 sleep 10
-nohup bin/kafka-server-start.sh config/server.properties &
+sudo nohup bin/kafka-server-start.sh config/server.properties &
+fi
