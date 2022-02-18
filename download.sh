60960c2e7facbe45ac5fc1aa386d0bfeb64e1b08
helmi <hichrihelmi@gmail.com>
2022-02-18 14:21:39 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
-sudo su
-mkdir -p /SERVICE/kafka
-cd /SERVICE/kafka
-wget https://miroir.univ-lorraine.fr/apache/kafka/2.4.1/kafka_2.12-2.4.1.tgz
-tar -xvf kafka_2.12-2.4.1.tgz
+
+sudo kdir -p /SERVICE/kafka
+sudo cd /SERVICE/kafka
+sudo wget https://archive.apache.org/dist/kafka/2.4.1/kafka_2.12-2.4.1.tgz
+sudo tar -xvf kafka_2.12-2.4.1.tgz
